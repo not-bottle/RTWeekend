5a4b479ecd1cea59d7e7c1ca47f36a8399350ccf
u6949034 <u6949034@anu.edu.au>
2024-04-02 14:22:58 +1100

    Antialiasing added

--- camera.h
+++ camera.h
@@ -12,6 +12,7 @@ class camera {
   public:
     double aspect_ratio = 1.0; // Ratio of image width over height
     int image_width = 100; // Rendered image width in pixel count
+    int samples_per_pixel = 10; // Count of random samples for each pixel
 
     void render(const hittable& world) {
         initialize();
@@ -23,12 +24,13 @@ class camera {
             std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 
             for (int i = 0; i < image_width; ++i) {
-                auto pixel_centre = pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
-                auto ray_direction = pixel_centre - centre;
+                colour pixel_colour(0, 0, 0);
+                for (int sample = 0; sample < samples_per_pixel; ++sample) {
+                    ray r = get_ray(i, j);
+                    pixel_colour += ray_colour(r, world);
+                }
 
-                ray r{centre, ray_direction}; // Ratios u and v are used to portion the viewport
-                colour pixel_colour = ray_colour(r, world);
-                write_colour(std::cout, pixel_colour);
+                write_colour(std::cout, pixel_colour, samples_per_pixel);
             }
         }
         std::cerr << "\nDone.\n";
@@ -84,6 +86,25 @@ class camera {
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5*(unit_direction.y() + 1.0);
         return (1.0-a)*colour(1.0, 1.0, 1.0) + a*colour(0.5, 0.7, 1.0);
+    }
+
+    ray get_ray(int i, int j) const {
+        // Get a randomly sampled camera ray for the pixel located at i, j
+        auto pixel_centre = pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
+        auto pixel_sample = pixel_centre + pixel_sample_square();
+
+        auto ray_origin = centre;
+        auto ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 pixel_sample_square() const {
+        // Returns a random point within a pixel (or the square surrounding the pixel's centre)
+
+        auto px = -0.5 + random_double();
+        auto py = -0.5 + random_double();
+        return (px * pixel_delta_u) + (px * pixel_delta_v);
     }
 };
 
--- colour.h
+++ colour.h
@@ -5,11 +5,22 @@
 #include <iostream>
 
 /* Writes colour to output stream in PPM format. */
-void write_colour(std::ostream &out, colour pixel_colour) {
+void write_colour(std::ostream &out, colour pixel_colour, int samples_per_pixel) {
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    // Divide the colour by the number of samples
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
-        << static_cast<int>(255.999 * pixel_colour.y()) << ' '
-        << static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+    static const interval intensity(0.000, 0.999);
+    out << static_cast<int>(255.999 * intensity.clamp(r)) << ' '
+        << static_cast<int>(255.999 * intensity.clamp(g)) << ' '
+        << static_cast<int>(255.999 * intensity.clamp(b)) << '\n';
 }
 
 #endif
